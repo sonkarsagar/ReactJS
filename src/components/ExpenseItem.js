0e940003bea9c8e1af42d82c09d6327a5ec0c41a
Sagar SOnkar <sagarsonker56625@gmail.com>
2023-10-06 16:47:02 +0530

    File Structure and Fat Arrow Functions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,15 +3,6 @@ import ExpenseDetails from './ExpenseDetails'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-//   return (
-//     <div className='expense-item'>
-//       <ExpenseDate date={props.date} />
-//       <div className='expense-item__description'>
-//         <h2>{props.title}</h2>
-//         <div className='expense-item__price'>${props.amount}</div>
-//       </div>
-//     </div>
-//   );
   return (
     <div className='expense-item'>
         <ExpenseDate date ={props.date} />
